6c6bb35d2a72bdb1b52c23b9f10b65197157c3a1
nmearl <nchlsearl@gmail.com>
2017-05-10 13:58:26 -0400

    More exploration of MCMC optimizer

--- spectacle/modeling/optimizers.py
+++ spectacle/modeling/optimizers.py
@@ -25,25 +25,32 @@ class OptimizerFitter:
 class PosteriorFitter(OptimizerFitter):
     supported_constraints = ['bounds', 'eqcons', 'ineqcons', 'fixed', 'tied']
 
-    def ln_likelihood(self, params, model, x, y, yerr):
+    @staticmethod
+    def ln_likelihood(params, model, x, y, yerr):
         _fitter_to_model_params(model, params)
+
+        print(".", end=" ")
+
         y_mod = model(x).data
 
-        inv_sigma2 = 1.0 / (yerr ** 2 + y_mod ** 2 * np.exp(2))
+        # inv_sigma2 = 1.0 / (yerr ** 2 + y_mod ** 2 * np.exp(2))
         # print("inv_sigma: ", inv_sigma2.sum(), min(y_mod))
         #
         # diff = (y - y_mod)
         # print("diff: ", diff.sum())
 
-        res = -0.5 * (np.sum(
-            (y - y_mod) ** 2 * inv_sigma2 - np.log(inv_sigma2)))
+        inv_sigma2 = 1.0/(yerr**2 + y_mod**2*np.exp(2))
+        return -0.5*(np.sum((y-y_mod)**2*inv_sigma2 - np.log(inv_sigma2)))
 
-        if np.isnan(res):
-            print(list(zip(model.param_names, model.parameters)))
+        # res = -0.5 * (np.sum((y - y_mod) ** 2 * inv_sigma2 - np.log(inv_sigma2)))
+        #
+        # if np.isnan(res):
+        #     print(list(zip(model.param_names, model.parameters)))
 
-        return res
+        # return res
 
-    def ln_prior(self, params, model):
+    @staticmethod
+    def ln_prior(params, model):
         _, fit_inds = _model_to_fit_params(model)
 
         # Check bounds. Assuming the order in theta is the same as parameters.
@@ -52,13 +59,14 @@ class PosteriorFitter(OptimizerFitter):
             return 0.0
         return -np.inf
 
-    def objective_function(self, params, model, x, y, yerr):
-        lp = self.ln_prior(params, model)
+    @staticmethod
+    def objective_function(params, model, x, y, yerr):
+        lp = PosteriorFitter.ln_prior(params, model)
 
         if not np.isfinite(lp):
             return -np.inf
 
-        return lp + self.ln_likelihood(params, model, x, y, yerr)
+        return PosteriorFitter.ln_likelihood(params, model, x, y, yerr)
 
     def __call__(self, model, x, y, yerr=None, *args, **kwargs):
         model_copy = _validate_model(model, self.supported_constraints)
@@ -69,31 +77,39 @@ class PosteriorFitter(OptimizerFitter):
 
         ndim, nwalkers = len(init_values), 100
 
-        pos = [init_values + 1e-4 * np.random.randn(ndim) for i in
+        pos = [init_values * (1 + 1e-4 * np.random.randn(ndim)) for i in
                range(nwalkers)]
 
-        self.sampler = emcee.EnsembleSampler(nwalkers, ndim,
-                                             self.objective_function,
-                                             args=(model_copy, x, y, yerr))
+        sampler = emcee.EnsembleSampler(nwalkers, ndim,
+                                        PosteriorFitter.objective_function,
+                                        args=(model_copy, x, y, yerr))
 
         # Burn-in
-        pos, prob, state = self.sampler.run_mcmc(pos, 200)
+        pos, prob, state = sampler.run_mcmc(pos, 200, rstate0=np.random.get_state())
 
         # reset the sampler
-        self.sampler.reset()
+        sampler.reset()
 
         # Run sampler
-        self.sampler.run_mcmc(pos, 500, rstate0=state)
+        sampler.run_mcmc(pos, 500, rstate0=state)
 
-        samples = self.sampler.chain[:, :, :].reshape((-1, ndim))
+        samples = sampler.chain[:, :, :].reshape((-1, ndim))
 
         # Compute the quantiles.
-        samples[:, 2] = np.exp(samples[:, 2])
+        # samples[:, 2] = np.exp(samples[:, 2])
+        import matplotlib.pyplot as plt
+        fig, axes = plt.subplots(3, 1, sharex=True, figsize=(8, 9))
+        axes[0].plot(sampler.chain[:, :, 2].T, color="k", alpha=0.4)
+        axes[0].set_ylabel("$m$")
+        plt.savefig("para_output.png")
 
-        fit_params = map(
-            lambda v: v,
+
+        fit_params = list(map(
+            lambda v: v[0],
             zip(*np.percentile(samples, [16, 50, 84],
-                               axis=0)))
+                               axis=0))))
+
+        print(fit_params)
 
         _fitter_to_model_params(model_copy, fit_params)
 
