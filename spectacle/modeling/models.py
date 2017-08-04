fb9663f863e31dd35e5b31d9ae228b70b459a331
nmearl <contact@nicholasearl.me>
2017-08-04 15:05:02 -0400

    Incorporate profile directly inside model

--- spectacle/modeling/models.py
+++ spectacle/modeling/models.py
@@ -4,7 +4,9 @@ from astropy.modeling.core import _ModelMeta
 from astropy.modeling.models import Linear1D
 import numpy as np
 
-from ..core.profiles import TauProfile
+import astropy.units as u
+from astropy import constants as c
+from scipy import special
 
 import six
 
@@ -13,26 +15,84 @@ class Voigt1D(Fittable1DModel):
     """
     Implements a Voigt profile (convolution of Cauchy-Lorentz and Gaussian
     distribution).
+
+    Create an optical depth vs. wavelength profile for an
+    absorption line using a voigt profile. This follows the paradigm of
+    the :func:`~yt.analysis_modules.absorption_spectrum.absorption_line`
+    profile generator.
+
+    Parameters
+    ----------
+    lambda_0 : float
+       Central wavelength in Angstroms.
+    f_value : float
+       Absorption line oscillator strength.
+    gamma : float
+       Absorption line gamma value.
+    v_doppler : float
+       Doppler b-parameter in cm/s.
+    column_density : float
+       Column density in cm^-2.
+    delta_v : float
+       Velocity offset from lambda_0 in cm/s. Default: None (no shift).
+    delta_lambda : float
+        Wavelength offset in Angstrom. Default: None (no shift).
+    lambda_bins : array-like
+        Wavelength array for line deposition in Angstroms. If None, one will be
+        created using n_lambda and dlambda. Default: None.
+    n_lambda : int
+        Size of lambda bins to create if lambda_bins is None. Default: 12000.
+    dlambda : float
+        Lambda bin width in Angstroms if lambda_bins is None. Default: 0.01.
     """
     lambda_0 = Parameter(fixed=True, min=0)
     f_value = Parameter(fixed=True, min=0, max=2.0)
     gamma = Parameter(fixed=True, min=0)
     v_doppler = Parameter(default=1e5, min=0)
     column_density = Parameter(default=13, min=0, max=25)
-    delta_v = Parameter(default=0, fixed=True)
-    delta_lambda = Parameter(default=0, fixed=False)
+    delta_v = Parameter(default=0, fixed=False)
+    delta_lambda = Parameter(default=0, fixed=True)
 
     def evaluate(self, x, lambda_0, f_value, gamma, v_doppler, column_density,
                  delta_v, delta_lambda):
-        profile = TauProfile(x, lambda_0=lambda_0, f_value=f_value,
-                             gamma=gamma, v_doppler=v_doppler,
-                             column_density=column_density,
-                             n_lambda=x.size,# lambda_bins=lambda_bins,
-                             delta_v=delta_v, delta_lambda=delta_lambda)
+        charge_proton = u.Quantity(4.8032056e-10, 'esu')
+        tau_factor = ((np.sqrt(np.pi) * charge_proton ** 2 /
+                       (u.M_e.cgs * c.c.cgs))).cgs
 
-        flux = np.exp(-profile.optical_depth) - 1.0
+        # Make the input parameters quantities so we can keep track
+        # of units
+        lambda_bins = x * u.Unit('Angstrom')
+        lambda_0 = lambda_0 * u.Unit('Angstrom')
+        v_doppler = v_doppler * u.Unit('cm/s')
+        column_density = 10 ** column_density * u.Unit('1/cm2')
+        delta_v = (delta_v or 0) * u.Unit('cm/s')
+        delta_lambda = (delta_lambda or 0) * u.Unit('Angstrom')
 
-        return flux
+        # shift lambda_0 by delta_v
+        shifted_lambda = lambda_0 * (1 + delta_v / c.c.cgs) + delta_lambda
+
+        # conversions
+        nudop = (v_doppler / shifted_lambda).to('Hz')  # doppler width in Hz
+
+        # tau_0
+        tau_X = tau_factor * column_density * f_value / v_doppler
+        tau0 = (tau_X * lambda_0).decompose()
+
+        # dimensionless frequency offset in units of doppler freq
+        x = c.c.cgs / v_doppler * (shifted_lambda / lambda_bins - 1.0)
+        a = gamma / (4.0 * np.pi * nudop)  # damping parameter
+        phi = self.voigt(a, x)  # line profile
+        tau_phi = tau0 * phi  # profile scaled with tau0
+        tau_phi = tau_phi.decompose().value
+
+        return tau_phi
+
+    @classmethod
+    def voigt(cls, a, u):
+        x = np.asarray(u).astype(np.float64)
+        y = np.asarray(a).astype(np.float64)
+
+        return special.wofz(x + 1j * y).real
 
     @staticmethod
     def fit_deriv(x, x_0, b, gamma, f):
