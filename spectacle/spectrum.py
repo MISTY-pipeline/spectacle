bc779735432af6554882d4bc1286138de5110615
nmearl <contact@nicholasearl.me>
2017-08-21 14:15:48 -0400

    Incorporate automatic velocity space/wavelength space switching

--- spectacle/spectrum.py
+++ spectacle/spectrum.py
@@ -15,17 +15,35 @@ class SpectrumModelNotImplemented(Exception): pass
 class IncompleteLineInformation(Exception): pass
 
 
-class Spectrum1D(Fittable1DModel):
-    num_lines = Parameter(default=0)
+class SpectrumModel(Fittable1DModel):
+    outputs = ('x',)
+    input_units_strict = True
+
+    center = Parameter(default=0, fixed=True, unit=u.Angstrom)
 
     def __init__(self, *args, **kwargs):
-        super(Spectrum1D, self).__init__(*args, **kwargs)
-        self._redshift_model = RedshiftScaleFactor(0)
+        super(SpectrumModel, self).__init__(*args, **kwargs)
+        self._redshift_model = RedshiftScaleFactor(0).inverse
         self._continuum_model = Linear1D(0, 1)
         self._line_model = None
 
+    @property
+    def input_units_equivalencies(self):
+        return {'x': [
+            (u.Unit('km/s'), u.Angstrom,
+             lambda x: WavelengthConvert(self.center.value)(x),
+             lambda x: VelocityConvert(self.center.value)(x))
+        ]}
+
+    @property
+    def input_units(self, *args, **kwargs):
+        return {'x': u.Unit('km/s')}
+
+    def evaluate(self, x, center, *args, **kwargs):
+        return x.to('Angstrom', equivalencies=self.input_units_equivalencies['x'])
+
     def copy(self):
-        new_spectrum = Spectrum1D()
+        new_spectrum = super(SpectrumModel, self).copy()
         new_spectrum._redshift_model = self._redshift_model.copy()
         new_spectrum._continuum_model = self._continuum_model.copy()
 
@@ -35,10 +53,15 @@ class Spectrum1D(Fittable1DModel):
         return new_spectrum
 
     def _change_base(self, base):
-        new_spectrum = self.copy()
+        new_spectrum = self.copy() | base
 
-        new_spectrum.__class__ = type('Spectrum1D', (Spectrum1D, type(base)),
-                                      base.__dict__)
+        # Add the required input validation steps to the compound model, as
+        # Astrop does not currently support adding these attributes to the
+        # compound model automatically.
+        setattr(new_spectrum, "input_units_strict", True)
+        setattr(new_spectrum, "input_units_equivalencies",
+                self.input_units_equivalencies)
+        setattr(new_spectrum, "input_units", self.input_units)
 
         return new_spectrum
 
@@ -55,7 +78,6 @@ class Spectrum1D(Fittable1DModel):
     def add_line(self, lambda_0=None, gamma=None, f_value=None,
                  column_density=None, v_doppler=None, delta_v=None,
                  delta_lambda=None, name=None, *args, **kwargs):
-
         if lambda_0 is None and name is None:
             raise IncompleteLineInformation(
                 "Not enough information to construction absorption line "
@@ -72,45 +94,29 @@ class Spectrum1D(Fittable1DModel):
 
     @property
     def tau(self):
-        new_compound = (RedshiftScaleFactor(self.redshift).inverse
-                        | self._line_model()
-                        | Scale(1. / (1 + self.redshift)))
+        new_compound = (self._redshift_model
+                        | self._line_model
+                        | Scale(1. / (1 + self._redshift_model.z.value)))
 
         return self._change_base(new_compound)
 
     @property
     def flux(self):
-        new_compound = (RedshiftScaleFactor(self.redshift).inverse
-                        | (Linear1D(0.0, 1.0)
-                           + (self._line_model() | FluxConvert()))
-                        | Scale(1. / (1 + self.redshift)))
+        new_compound = (self._redshift_model
+                        | (self._continum_model
+                           + (self._line_model | FluxConvert()))
+                        | Scale(1. / (1 + self._redshift_model.z.value)))
 
         return self._change_base(new_compound)
 
     @property
     def flux_decrement(self):
-        new_compound = (RedshiftScaleFactor(self.redshift).inverse
-                        | (Linear1D(0.0, 1.0)
-                           + (self._line_model() | FluxDecrementConvert()))
-                        | Scale(1. / (1 + self.redshift)))
+        new_compound = (self._redshift_model
+                        | (self._continum_model
+                           + (self._line_model | FluxDecrementConvert()))
+                        | Scale(1. / (1 + self._redshift_model.z.value)))
 
         return self._change_base(new_compound)
-
-    def velocity_space(self, velocity, center):
-        if issubclass(type(self), _CompoundModel):
-            return (WavelengthConvert(center) | self)(velocity)
-        else:
-            raise SpectrumModelNotImplemented(
-                "No spectral axis specified. Please method chain with `tau`, "
-                "`flux`, or `flux_decrement`.")
-
-    def wavelength_space(self, wavelength):
-        if issubclass(type(self), _CompoundModel):
-            return self(wavelength)
-        else:
-            raise SpectrumModelNotImplemented(
-                "No spectral axis specified. Please method chain with `tau`, "
-                "`flux`, or `flux_decrement`.")
 
     def line_mask(self, x):
         masks = [line.mask(x) for line in self._lines]
