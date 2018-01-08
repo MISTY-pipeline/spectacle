16ae7407fcea045f9a37783d7becca566dd54d8d
Nicholas Earl <contact@nicholasearl.me>
2018-01-08 00:23:42 -0500

    Update base package information

--- spectacle/__init__.py
+++ spectacle/__init__.py
@@ -1,3 +1,9 @@
+"""Top-level package for spectacle."""
+
 import logging
+
+__author__ = """Nicholas Earl"""
+__email__ = 'nearl@stsci.edu'
+__version__ = '0.1.0'
 
 logging.basicConfig(level=logging.INFO)
