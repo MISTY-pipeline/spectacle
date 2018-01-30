d42b7728694be2f4c83962b6e0a222927e03aa16
nmearl <contact@nicholasearl.me>
2018-01-30 15:17:57 -0500

    Format logging

--- spectacle/__init__.py
+++ spectacle/__init__.py
@@ -6,4 +6,5 @@ __author__ = """Nicholas Earl"""
 __email__ = 'nearl@stsci.edu'
 __version__ = '0.1.0'
 
-logging.basicConfig(level=logging.INFO)
+logging.basicConfig(format='spectacle [%(levelname)-8s]: %(message)s',
+                    level=logging.INFO)
