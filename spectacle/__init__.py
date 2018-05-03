e3d28f4ef1c931ea180f90d4b60bec56a36a7653
nmearl <contact@nicholasearl.me>
2018-05-03 10:16:48 -0400

    Bump version: 0.1.0 → 0.2.0

--- spectacle/__init__.py
+++ spectacle/__init__.py
@@ -4,7 +4,7 @@ import logging
 
 __author__ = """Nicholas Earl"""
 __email__ = 'nearl@stsci.edu'
-__version__ = '0.1.0'
+__version__ = '0.2.0'
 
 logging.basicConfig(format='spectacle [%(levelname)-8s]: %(message)s',
                     level=logging.INFO)
