5148ce4aa1020e81f7b7fc0da0817daa1f35244a
nmearl <contact@nicholasearl.me>
2018-11-19 12:14:01 -0500

    Include line spread functions

---
+++ spectacle/modeling/lsfs.py
@@ -0,0 +1,70 @@
+import os
+
+from astropy.convolution import Gaussian1DKernel, Kernel1D, convolve
+from astropy.modeling import Fittable1DModel, Parameter
+from astropy.table import Table
+
+__all__ = ['COSLSFModel', 'GaussianLSFModel']
+
+
+class COSKernel1D(Kernel1D):
+    """
+    1D kernel filter for the COS instrument.
+    """
+    _separable = True
+    _is_bool = False
+
+    def __init__(self):
+        path = os.path.abspath(
+            os.path.join(__file__, '..', '..', 'data', 'cos.ecsv'))
+        table = Table.read(path, format='ascii.ecsv')
+
+        super(COSKernel1D, self).__init__(array=table['value'])
+
+
+class LSFModel(Fittable1DModel):
+    inputs = ('y',)
+    outputs = ('y',)
+
+    def __init__(self, kernel=None, *args, **kwargs):
+        super(LSFModel, self).__init__(*args, **kwargs)
+
+        self._kernel = kernel
+
+    @property
+    def kernel(self):
+        return self._kernel
+
+    @kernel.setter
+    def kernel(self, value):
+        self._kernel = value
+
+    def evaluate(self, y):
+        return convolve(y, self.kernel, boundary='extend')
+
+
+class COSLSFModel(LSFModel):
+    """
+    COS LSF model which can be used with the compound model objects.
+    """
+    inputs = ('y',)
+    outputs = ('y',)
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(kernel=COSKernel1D(), *args, **kwargs)
+
+
+class GaussianLSFModel(LSFModel):
+    """
+    Gaussian LSF model which can used with the compound model objects.
+    """
+    inputs = ('y',)
+    outputs = ('y',)
+
+    stddev = Parameter(default=0, min=0, fixed=True)
+
+    def __init__(self, stddev, *args, **kwargs):
+        super().__init__(kernel=Gaussian1DKernel(stddev, *args, **kwargs))
+
+        self._kernel_args = args
+        self._kernel_kwargs = kwargs
