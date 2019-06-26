938b982adf68d1b84fa9ab71ad363f3377402fb4
nmearl <contact@nicholasearl.me>
2019-06-26 13:57:02 -0400

    Add test for custom continuum

--- spectacle/tests/test_model.py
+++ spectacle/tests/test_model.py
@@ -2,6 +2,7 @@ import pytest
 from ..modeling import Spectral1D, OpticalDepth1D
 import numpy as np
 import astropy.units as u
+from astropy.modeling.models import Linear1D
 
 
 def test_create_model():
@@ -17,6 +18,24 @@ def test_create_model():
     x = np.linspace(-100, 100, 200) * u.AA
 
     assert np.allclose(spec_mod_lambda(x), spec_mod_name(x))
+
+
+def test_custom_continuum():
+    line1 = OpticalDepth1D(lambda_0=1216 * u.AA)
+    continuum = Linear1D(slope=1 * u.s/u.km, intercept=0 * u.Unit(''))
+
+    # Create model from lambda
+    spec_mod = Spectral1D(line1, continuum=continuum)
+
+    wav = np.linspace(1100, 1300, 500) * u.AA
+    vel = np.linspace(-100, 100, 500) * u.km/u.s
+
+    # Test in wavelength and velocity spaces
+    assert len(spec_mod(wav)) == 500
+    assert len(spec_mod(vel)) == 500
+
+    assert np.trapz(spec_mod(wav), x=wav) > 0
+    assert np.trapz(spec_mod(vel), x=vel) > 0
 
 
 def test_add_line():
